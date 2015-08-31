3159a13d415fe833d8930e74cb017beb564a1e5f
Sanjeev Ojha <sanjeevojha@Sanjeev-Ojhas-MacBook-Pro.local>
2015-08-30 18:53:13 -0700

    Favicon and PRoject fix added

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,14 +4,18 @@ $(function () {
 
 $(document).ready(function(){
     $("#one").click(function(){
-        $("#two,#three").toggle();
+       /* $("#two,#three").toggle();*/
         $("#about").slideToggle("slow");
     });
 
-    $("#two").click(function(){
-        $("#one,#three").slideToggle("slow");
-        $("#projetDivRight,#projetDivLeft").slideToggle("slow");
+    $("#about").click(function(){
+    	$(this).slideToggle("slow");
     });
+
+    $("#two").click(function(){
+        $("#one,#three,#projetDivRight,#projetDivLeft").toggle();
+/*        $("#projetDivRight,#projetDivLeft").fadeToggle();
+*/  });
 
     $("#three").click(function(){
     	$(this).find('img').toggle();
