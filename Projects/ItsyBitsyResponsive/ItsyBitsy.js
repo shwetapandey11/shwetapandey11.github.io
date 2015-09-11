a6e40e8246d808d49c3eb3405cb62fd056032b3b
Sanjeev Ojha <sanjeevojha@Sanjeev-Ojhas-MacBook-Pro.local>
2015-09-11 11:17:32 -0700

    checkbox cart

--- Projects/ItsyBitsyResponsive/ItsyBitsy.js
+++ Projects/ItsyBitsyResponsive/ItsyBitsy.js
@@ -34,8 +34,22 @@ $(document).ready(function(){
 				} else {
 					html += "<div class='itemImage'><img src='images/Frock1.jpg'/></div>";
 				}
-				html += "<div class='itemPrice'>" + eachItem.price + "</div></li>";
-			});
+				html += "<div class='itemPrice'>" + eachItem.price + "</div>";
+
+
+				html += "<div class='checkbox'><p class='checkboxCookie text-center'><i class='fa fa-cart-plus'></i><input type='checkbox' id='checkboxCart'/></p></div></li>";
+
+
+				/*var info = "<i class='fa fa-info-circle'></i>";
+
+				if( $("#checkboxCart").is(':checked')){
+					$(info).show();
+				}else{
+					$(info).hide();
+				}*/
+
+				
+			});  
 			element.append(html);
 
 
@@ -62,3 +76,64 @@ $(document).ready(function(){
 });
 
 
+/*----------------------------------  cookie ---------------------------------------*/
+
+function setCookie(cart,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cart+"="+cvalue+"; "+expires;
+}
+
+function getCookie(cname) {
+    var name = cart + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+    }
+}
+
+
+function isItemInCart (itemId){
+	var cookies = getCookie("cart");
+	if (cookies === ""){
+		return false;
+	} else {
+		if (cookies.indexOf(itemId) == -1){
+			return false;
+		}
+	}
+	return true;
+}
+
+function addItemToCookie(itemId){
+	if (!isItemInCart){
+		var cookies = getCookie("cart");
+		cookies += ":"  + itemId;
+		setCookie("cart", cookies);
+	}
+}
+
+function remoteItemFromCookie(itemId){
+	if (isItemInCart){
+		var cookies = getCookie("cart");
+		
+	}
+}
